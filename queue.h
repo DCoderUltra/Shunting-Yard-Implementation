b3dc92dab81cfe6a56b45930b7692aa8c1576e30
DCoderUltra <82941044+DCoderUltra@users.noreply.github.com>
2023-04-03 15:45:07 +0000

    Delete queue.h

--- queue.h
+++
@@ -1,8 +0,0 @@
-// Queue Header File
-void init_q(QE ** queue);
-int empty_q(QE * queue);
-void enqueue(QE ** queue, TOKEN tok);
-void dequeue(QE ** queue);
-void list_q(QE * queue);
-TOKEN first(QE * queue);
-
